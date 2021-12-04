c61ac5a1904452b31640742dadebd3db1406dee7
HRSHDG8 <hrshdg8@gmail.com>
2021-12-03 17:45:39 -0800

    lab 13

--- src/main/java/edu/sdsu/cs160l/Main.java
+++ src/main/java/edu/sdsu/cs160l/Main.java
@@ -1,17 +1,13 @@
 package edu.sdsu.cs160l;
 
 /**
- * TODO
+ * TODO (the final one :) )
  * Follow the steps listed below
  * Your programming assignment is worth 6 points this lab and easy to score
- * 1) Refer to BubbleSort and QuickSort Examples and
- *  i)      create a new class called MergeSort.java.
- *  ii)     class MergeSort must implement Sorter interface.
- *  iii)    you must implement merge sort logic (you can refer to code in zybook)
- * 2) Find TODO's in AlgorithmTimeComparators and MergeSortTest and correct it.
- * 3) For your lab report
- *  i) explain time complexity of an algorithm
- *  ii) compare time complexity of merge sort with quick sort and bubble sort
+ * 1) Refer to BubbleSort and QuickSort Examples and convert your MergeSort class into a generic one.
+ * 2) For your lab report
+ *   explain in your own words the use of generics to reduce duplicate code.
+ *
  */
 public class Main {
 }
