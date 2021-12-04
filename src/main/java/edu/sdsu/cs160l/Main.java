0d70ff3aa97297cd1dcfed0b0b5da81efc5c17e7
HRSHDG8 <hrshdg8@gmail.com>
2021-12-03 17:46:28 -0800

    lab 13

--- src/main/java/edu/sdsu/cs160l/Main.java
+++ src/main/java/edu/sdsu/cs160l/Main.java
@@ -6,7 +6,7 @@ package edu.sdsu.cs160l;
  * Your programming assignment is worth 6 points this lab and easy to score
  * 1) Refer to BubbleSort and QuickSort Examples and convert your MergeSort class into a generic one.
  * 2) For your lab report
- *   explain in your own words the use of generics to reduce duplicate code.
+ *   explain the use of generics to reduce duplicate code.
  *
  */
 public class Main {
